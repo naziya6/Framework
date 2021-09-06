b37a1e44075a1f008a8b1e20ec50ab90c49a6b8e
naziya6 <naziya6riyaz@gmail.com>
2021-09-06 12:24:13 +0100

    framework

---
+++ APIAutomation/Api/BillingOrder.cs
@@ -0,0 +1,60 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIAutomation.Api
+{
+  
+    class BillingOrder
+    {
+        string baseurl = "http://localhost:8080/BillingOrder";
+        public IRestResponse GetAll()
+        {
+            var client = new RestClient(baseurl)
+            //Request post or get
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            //Execution
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+        public IRestResponse Post(string body) {
+            var client = new RestClient(baseurl);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(body);
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+
+        public IRestResponse Put(int id, string body)
+        {
+            var client = new RestClient(baseurl + "/" + id);
+            var request = new RestRequest(Method.PUT);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(body);
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+            public IRestResponse Delete(int id)
+        {
+            var client = new RestClient(baseurl + "/" + id);
+            var request = new RestRequest(Method.DELETE);
+            request.AddHeader("Content-Type", "application/json");
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+        public IRestResponse Get(int id)
+        {
+            var client = new RestClient(baseurl + "/" + id);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+    }
+}
---
+++ APIAutomation/BillingOrder test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using RestSharp;
+
+namespace APIAutomation
+{
+    class Billingorder_test
+    {
+        string jsonBody = "{\"addressLine1\":\"address1\",\"addressLine2\":\"address2\",\"city\":\"london\",\"comment\":\"test comment\"," +
+            "\"email\":\"naziya6@gmail.com\",\"firstName\":\"naziya\",\"itemNumber\":0,\"lastName\":\"abdul\",\"phone\":\"1234567891\",\"state\":\"AL\",\"zipCode\":\"121212\"}";
+        string baseurl = "http://localhost:8080/BillingOrder";
+
+        [Test]
+        public void CreateBillingOrder()
+        {
+            IRestResponse response = Post(jsonBody);
+
+            //printing
+            TestContext.WriteLine(response.StatusCode);
+            TestContext.WriteLine(response.Content);
+        }
+
+
+
+        public IRestResponse Post(string body)
+        {
+            var client = new RestClient(baseurl);
+
+            //Request post or get
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(body);
+
+            //Execution
+            IRestResponse response = client.Execute(request);
+            return response;
+
+
+        }
+
+
+
+    }
+}
+
+
+
+
